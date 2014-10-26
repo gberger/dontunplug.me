a66a1e878004818c6876f9dec415ee16d8a8df04
Guilherme Berger <guilherme.berger@gmail.com>
2014-10-25 21:28:44 -0400

    hazard symbol

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
 var port = process.env.PORT || 9000;
 
 app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.json());
+
+app.post('/alert', function(req, res) {
+	console.log(req.body);
+	res.end(200);
+});
 
 app.listen(port);
