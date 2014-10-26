f4cc544561163f00c3e63b25ddbb5cce50433c87
Guilherme Berger <guilherme.berger@gmail.com>
2014-10-26 01:17:56 -0400

    send alerts

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ var bodyParser = require('body-parser')
 var app = express();
 var port = process.env.PORT || 9000;
 
+var mobileAlert = require('./lib/mobile-alert');
+var emailAlert = require('./lib/email-alert');
+
 app.use(express.static(__dirname + '/static'));
 app.use(bodyParser.json());
 
 app.post('/alert', function(req, res) {
-	console.log(req.body);
+	mobileAlert(req.body);
+	emailAlert(req.body);
 	res.sendStatus(200);
 });
 
